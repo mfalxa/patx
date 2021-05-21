5038b7e6436109002f7f173447811ae43f81f21e
Falxa Mikel <falxa@apc.in2p3.fr>
2021-05-22 00:39:00 +0200

    update window

--- main.js
+++ main.js
@@ -13,10 +13,9 @@ function displayUL(event) {
 
 function updatePlotSizes(){
     var plotWidth = 49*document.getElementById(0).width + document.getElementById('corner').width;
-    var k = (window.innerWidth - 200) / plotWidth
-    document.getElementById('corner').width = Math.floor(k * document.getElementById('corner').width);
+    document.getElementById('corner').width = Math.floor((window.innerWidth-200) * 0.5);
     for (var i =0; i < 49; i++) {
-        document.getElementById(i).width = Math.floor(k * document.getElementById(i).width);
+        document.getElementById(i).width = Math.floor((window.innerWidth-200) * 0.5 / 49);
     }
 }
 
@@ -28,6 +27,7 @@ for (var i=0; i < 49; i++) {
     var img = document.createElement('img');
     img.id = i;
     img.src = './src/UL_0/ul' + i +'.png';
+    Math.floor((window.innerWidth-200) * 0.5 / 49)
     src.appendChild(img);
     }
 
@@ -35,6 +35,7 @@ var corner = document.getElementById('corner');
 var header = document.getElementById('header')
 src.addEventListener("mouseenter", function(event) {
     corner.src = './src/'+currentDir+'/corner/corner' + event.target.id +'.jpg';
+    corner.width = Math.floor((window.innerWidth-200) * 0.5)
     header.innerHTML = 'f = 1e' + data['f'][event.target.id].toFixed(2) + ' Hz || log_ul = ' + data[currentDir]['ul_'+event.target.id].toFixed(2) + ' || log_median = ' + data[currentDir]['medians_'+event.target.id].toFixed(2)
     }, true);
 
