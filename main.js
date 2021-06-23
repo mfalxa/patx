9c27cdc6e922b043ed93d053597220c5ce653ca5
Falxa Mikel <falxa@apc.in2p3.fr>
2021-06-23 08:00:39 +0200

    add no crn

--- main.js
+++ main.js
@@ -4,10 +4,23 @@ function displayUL(event) {
         document.getElementById(i).remove();
         var img = document.createElement('img');
         img.id = i;
-        img.src = './src/'+event.target.id+'/ul' + i +'.png';
+        img.src = './src/'+dirPrefix+event.target.id+'/ul' + i +'.png';
         src.appendChild(img);
         }
     currentDir = event.target.id;
+    document.getElementById(currentDir).src = './src/sky/s'+currentDir+'.png';
+    updatePlotSizes();
+}
+
+function displayULCrn() {
+    document.getElementById(currentDir).src = './src/sky/'+currentDir+'.png';
+    for (var i=0; i < 49; i++) {
+        document.getElementById(i).remove();
+        var img = document.createElement('img');
+        img.id = i;
+        img.src = './src/'+dirPrefix+currentDir+'/ul' + i +'.png';
+        src.appendChild(img);
+        }
     document.getElementById(currentDir).src = './src/sky/s'+currentDir+'.png';
     updatePlotSizes();
 }
@@ -17,13 +30,31 @@ function updatePlotSizes(){
     var subPlots = document.getElementById('plot').childNodes;
     for (var i = 0; i < subPlots.length; i++) {
         subPlots[i].width = Math.floor((window.innerWidth) * 0.5 / 49);
-        console.log(subPlots[i].width)
+    }
+}
+
+function crnOnOff(event){
+    console.log(dirPrefix);
+    console.log(crn.innerHTML);
+    if (crn.innerHTML === 'Common red noise ON'){
+        crn.innerHTML = "Common red noise OFF";
+        dirPrefix = 'no_crn_';
+        displayULCrn();
+    }
+    else if (crn.innerHTML === 'Common red noise OFF'){
+        crn.innerHTML = 'Common red noise ON';
+        dirPrefix = '';
+        displayULCrn();
     }
 }
 
 window.addEventListener('resize', updatePlotSizes)
 
+var crn = document.getElementById('crn')
+crn.addEventListener('click', crnOnOff)
+
 var currentDir = 'UL_0'
+var dirPrefix = ''
 var src = document.getElementById('plot');
 for (var i=0; i < 49; i++) {
     var img = document.createElement('img');
@@ -35,9 +66,9 @@ for (var i=0; i < 49; i++) {
 var corner = document.getElementById('corner');
 var header = document.getElementById('header')
 src.addEventListener("mouseenter", function(event) {
-    corner.src = './src/'+currentDir+'/corner/corner' + event.target.id +'.jpg';
+    corner.src = './src/'+dirPrefix+currentDir+'/corner/corner' + event.target.id +'.jpg';
     updatePlotSizes();
-    header.innerHTML = 'f = 1e' + data['f'][event.target.id].toFixed(2) + ' Hz || log_ul = ' + data[currentDir]['ul_'+event.target.id].toFixed(2) + ' || log_median = ' + data[currentDir]['medians_'+event.target.id].toFixed(2)
+    header.innerHTML = 'f = 1e' + data['f'][event.target.id].toFixed(2) + ' Hz || log_ul = ' + data[currentDir][dirPrefix+'ul_'+event.target.id].toFixed(2) + ' || log_median = ' + data[currentDir][dirPrefix+'medians_'+event.target.id].toFixed(2)
     }, true);
 
 updatePlotSizes();
